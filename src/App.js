84d02c3132c8adf1f2b2ca8a507f239da7787c1b
ak2776 <abhinav.eec@gmail.com>
2021-12-30 20:56:37 +0530

    about component added

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import './App.css';
+import About from './components/About';
 import Navbar from './components/Navbar';
-import TextForm from './components/TextForm';
+// import TextForm from './components/TextForm';
 
 function App() {
+  // const [darkMode, setDarkMode] = useState(false);   //whether dark mode is enabled or not
   return (
     <>
-    <Navbar title="TextUtils" aboutUs="About us" />
+    <Navbar title="TextUtils" aboutUs="About us"/>
     {/* <Navbar /> */}
 
     <div className="container" my-5>
-    <TextForm heading="Enter the text" />
+    {/*<TextForm heading="Enter the text" /> */}
+    <About/>
     </div>
+    
     
     </>
   );
--- src/components/TextForm.js
+++ src/components/TextForm.js
@@ -7,23 +7,57 @@ export default function TextForm(props) {
     setText(newText);
   }
 
+  const handleLoClick = ()=> {
+    let newText = text.toLowerCase();
+    setText(newText);
+  }
+
+  const handleClClick = ()=> {
+    let newText = '';
+    setText(newText);
+  }
+
+  const handleCopy = () => {
+    var text = document.getElementById("myBox");
+    text.select();
+    navigator.clipboard.writeText(text.value);
+  }
+
   const handleOnChange = (event) => {
     // console.log('on change');
     setText(event.target.value);
   }
 
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/);
+    setText(newText.join(" "));
+  }
 
-  const[text, setText] = useState("Enter text here");
+  const[text, setText] = useState("");
   // text = 'new text';   Wrong way to change the state
   // setText("new text");   Right way to change the state
   return (
-    <div>
+    <>
+    <div className="container">
       <h1>{props.heading}</h1>
       <div className="mb-3">
         
         <textarea className="form-control" value={text} onChange={handleOnChange} id="myBox" rows="5"></textarea>
       </div>
-      <button className="btn btn-primary" onClick={handleUpClick}>To Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleUpClick}>To Uppercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleLoClick}>To Lowercase</button>
+      <button className="btn btn-primary mx-2" onClick={handleClClick}>To Clear</button>
+      <button className="btn btn-primary mx-2" onClick={handleCopy}>Copy Text</button>
+      <button className="btn btn-primary mx-2" onClick={handleExtraSpaces}>Remove ExtraSpace</button>
     </div>
+
+    <div className="container my-3">
+      <h2>Your text summary</h2>
+      <p>{text.split(" ").length} words and {text.length} characters</p>
+      <p>{0.008 * text.split(" ").length} Minutes read.</p>
+      <h3>Preview</h3>
+      <p>{text}</p>
+    </div>
+    </>
   );
 }
